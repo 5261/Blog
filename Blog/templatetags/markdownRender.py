ed9b615972041f7020695f3ffde76117dbf32436
5261 <461083145@qq.com>
2016-10-18 21:17:49 +0800

    add code highlight

--- Blog/templatetags/markdownRender.py
+++ Blog/templatetags/markdownRender.py
@@ -1,11 +1,26 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
-from mistune import markdown
+
+import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
 
 register = template.Library()
+
+class HighlightRenderer(mistune.Renderer):
+    def block_code(self, code, lang):
+        if not lang:
+            return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
+        lexer = get_lexer_by_name(lang, stripall = True)
+        formatter = html.HtmlFormatter(style = "monokai")
+        return highlight(code, lexer, formatter)
+
+renderer = HighlightRenderer()
+markdown = mistune.Markdown(renderer = renderer)
 
 @register.filter(name = "markdownRender", is_safe = True)
 @stringfilter
 def renderMarkdown(src):
-    return mark_safe(markdown(src, parse_block_html = True))
+    return mark_safe(markdown(src))
