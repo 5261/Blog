1742704bdcd12cc86bfda5767424b387cff47d34
5261 <461083145@qq.com>
2016-10-18 19:08:03 +0800

    change markdownRender to mistune

--- Blog/templatetags/markdownRender.py
+++ Blog/templatetags/markdownRender.py
@@ -1,11 +1,11 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
-from markdown import markdown
+from mistune import markdown
 
 register = template.Library()
 
 @register.filter(name = "markdownRender", is_safe = True)
 @stringfilter
 def renderMarkdown(src):
-    return mark_safe(markdown(src))
+    return mark_safe(markdown(src, parse_block_html = True))
